b9061593ba62ed16918108f136e76e5d9abb1104
indra <indrapokhrel15@gmail.com>
2026-01-30 08:28:44 +0545

    intheritance commit

---
+++ .idea/libraries/inheritance.dart
@@ -0,0 +1,28 @@
+class lo{
+  void feature() {
+  print("they inhales and exaheles");
+}
+}
+
+class animal extends lo{
+  void legs(){
+    print("have four legs");
+  }
+}
+class humans extends lo{
+  void legs(){
+    print("have 2 legs");
+  }
+}
+
+void main()
+{
+  animal a1 = animal();
+  a1.legs();
+  a1.feature();
+
+  humans h1 = humans();
+  h1.legs();
+  h1.feature();
+}
+
